493a25e7af839ff10c6718501e8a62d1bf9e3762
h3lax <axel.poidebard@gmail.com>
2025-02-01 19:08:46 +0100

    fix[metabse]:metabse se connecte bien à sa db maintenant et tourne correctement

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from app.routes import register_routes
 # Import models
 from app import models
 
-
 def create_app(config_class=Config):
     """Flask application factory pattern."""
     app = Flask(__name__)
@@ -24,14 +23,5 @@ def create_app(config_class=Config):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
-
-    
-    """ @app.route('/test-db')
-    def test_db():
-        db = get_db()
-        with db.cursor() as cursor:
-            cursor.execute('SELECT NOW();')
-            result = cursor.fetchone()
-        return {'current_time': result['now']} """
 
     return app
