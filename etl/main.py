182c599a8fae6551e4c75b5ae32e9b92133165bb
Anthony S <asteib.pro@gmail.com>
2025-02-16 13:14:03 +0100

    feat: main etl

--- etl/main.py
+++ etl/main.py
@@ -1,4 +1,25 @@
-from components.cli import main
+# from components.cli import main
+
+# if __name__ == "__main__":
+#     main()
+
+from components.extract import extract_csv
+from components.transformToDataBase import transform_data
+from components.loadToDataBase import load_data
+from configETL import CONFIG
+
+def run_etl():
+    print
+
+    print("🔄 Extraction et Transformation des fichiers...")
+    for file_key in CONFIG["datasets"]:
+        transform_data(file_key)
+
+    # print("🔄 Chargement des données dans PostgreSQL...")
+    # for file_key in CONFIG["files"]:
+    #     load_data(file_key)
+
+    print("✅ Pipeline terminé !")
 
 if __name__ == "__main__":
-    main()
+    run_etl()
