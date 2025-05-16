ac400afc672a5d85753e03c2264a4b75e85f29b7
h3lax <axel.poidebard@gmail.com>
2025-05-16 10:57:56 +0200

    set[CORS]:allow access from the app to request the api

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from app.db import db
 from flask_migrate import Migrate
+from flask_cors import CORS
 from app.routes import register_routes
 
 # Import models
@@ -10,6 +11,11 @@ from app import models
 def create_app(config_class=Config):
     """Flask application factory pattern."""
     app = Flask(__name__)
+
+    # Enable CORS for the entire app
+    CORS(app, origins=["http://localhost:5173"], methods=["GET", "POST"])
+
+    # Load configuration
     app.config.from_object(config_class) # We using the config.py
 
     # Initialize extensions
